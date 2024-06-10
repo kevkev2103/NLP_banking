677c5352ae96c35ee98e43bf6301064fa4418518
Melobgn <melodybugnon@gmail.com>
2024-06-10 16:24:39 +0200

    custom application gradio

--- app_gradio.py
+++ app_gradio.py
@@ -1,64 +1,3 @@
-# import gradio as gr
-# import pickle
-# from pymongo import MongoClient
-
-
-# # Charger les configurations et fonctions du modèle (Assurez-vous que ce code est dans le même script ou dans un module importé)
-# from model import Config, speech_to_text, analyze_text, analyze_with_llm, SpeechToTextAndNLPChain
-
-
-# config = Config()
-
-
-# cluster = MongoClient(config.mongodb_uri)
-# db = cluster[config.mongodb_db_name]
-# collection = db[config.mongodb_collection_name]
-
-# # Définir la fonction pour enregistrer les données dans MongoDB
-# def enregistrer_dans_mongodb(data):
-#     collection.insert_one(data)
-#     return "Données enregistrées dans MongoDB."
-
-# # Charger le modèle depuis le fichier .pkl
-# with open('transcription_data.pkl', 'rb') as f:
-#     model = pickle.load(f)
-
-# # Définir la fonction qui traite le fichier audio
-# def process_audio_file(audio_file_path):
-#     # Utiliser les fonctions de traitement pour analyser l'audio
-#     transcription = speech_to_text(audio_file_path)
-#     sentiment_analysis = analyze_text(transcription)
-#     llm_analysis = analyze_with_llm(transcription) if sentiment_analysis and sentiment_analysis["sentiment"] == "negative" else None
-    
-#     # Préparer les résultats
-#     result = {
-#         "transcription": transcription,
-#         "sentiment_analysis": sentiment_analysis,
-#         "llm_analysis": llm_analysis
-#     }
-#     enregistrer_dans_mongodb(result)
-#     return result
-
-# # def custom_button():
-# #     return gr.Button("Enregistrer dans MongoDB", enregistrer_dans_mongodb)
-
-# # Créer l'interface Gradio avec le bouton personnalisé pour MongoDB
-# interface = gr.Interface(
-#     fn=process_audio_file,
-#     inputs=gr.Audio(type="filepath"),
-#     outputs=gr.JSON(),
-#     title="Analyse de sentiments de clients dans le contexte bancaire",
-#     description="Chargez un fichier audio à analyser.",
-#     allow_flagging="never",
-#     theme='abidlabs/dracula_test',
-# )
-
-
-# # Lancer l'application Gradio
-# if __name__ == "__main__":
-#     interface.launch()
-
-
 import gradio as gr
 import pickle
 from pymongo import MongoClient
@@ -125,12 +64,14 @@ def custom_button():
 # Créer l'interface Gradio avec le bouton personnalisé pour MongoDB
 with gr.Blocks(theme="freddyaboulton/dracula_revamped") as interface:
     gr.Markdown("# Analyse de sentiments de clients dans le contexte bancaire")
+    gr.Image("img/handshake.jpg", label="Handshake Client", width=300, height=300)
     gr.Markdown("Chargez un fichier audio à analyser.")
     audio_input = gr.Audio(type="filepath", label="Fichier audio")
     result_output = gr.Markdown(label="Résultat")
     
     submit_button = gr.Button("Analyser")
     submit_button.click(process_audio_file, inputs=audio_input, outputs=result_output)
+
     
 
 
