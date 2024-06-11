0fe017b4c9a9176a794bf1da2de9105eba9d347a
Thibaut W <tibo7@gmail.com>
2024-06-11 15:52:53 +0200

    simplon update: sentiment-analysis model changed (now from Azure), everything's working + cleaning files

--- models/langchain.py
+++ models/langchain.py
@@ -1,32 +1,112 @@
-from .speech2text import SpeechToTextModel
-from .translation import TranslationModel, Speech2TextTranslationChain
-from .sentiment_analysis import SentimentAnalysisModel, SentimentAnalysisChain
+from pydantic import Field
+from langchain.chains.base import Chain
+from datetime import datetime
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import openai
+from .models import Speech2TextModel, SentimentAnalysisModel, GenerateResponseModel
 
-# Utilisation du modèle avec LangChain
-stt_model = SpeechToTextModel()
-translation_model = TranslationModel()
-collection = stt_model.collection  # Utilisation de la collection de SpeechToText
+# Chargement des variables d'environnement
+load_dotenv()
+speech_key = os.getenv("speech_key")
+region = os.getenv("region")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = os.getenv("OPENAI_API_BASE")
+openai.api_type = 'azure'
+openai.api_version = os.getenv("OPENAI_VERSION")
 
-# Créez les chaînes
-transcription_translation_chain = Speech2TextTranslationChain(stt_model=stt_model, translation_model=translation_model)
+# Configuration de MongoDB
+mongodb_uri = os.getenv("mongodb_uri")
+client = MongoClient(mongodb_uri)
+db = client["AzureSpeechToText"]
+collection = db["transcriptions"]
+
+# Vérification que toutes les variables d'environnement sont bien présentes
+if not all([speech_key, region, mongodb_uri]):
+    raise ValueError("One or more environment variables are missing")
+
+class DeepChain(Chain):
+    stt_model: Speech2TextModel = Field(...)
+    sentiment_model: SentimentAnalysisModel = Field(...)
+    generate_response_model: GenerateResponseModel = Field(...)
+    collection: pymongo.collection.Collection = Field(...)
+    speech_key = Field(...)
+    region = Field(...)
+    mongodb_uri = Field(...)
+
+    def __init__(self, stt_model, sentiment_model, generate_response_model, collection, speech_key, region, mongodb_uri, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stt_model = stt_model
+        self.sentiment_model = sentiment_model
+        self.generate_response_model = generate_response_model
+        self.collection = collection
+        load_dotenv()
+        self.speech_key = os.getenv("speech_key")
+        self.region = os.getenv("region")
+        self.mongodb_uri = os.getenv("mongodb_uri")
+        if not all([self.speech_key, self.region, self.mongodb_uri]):
+            raise ValueError("One or more environment variables are missing")
+
+    def _call(self, inputs):
+        audio_file_path = inputs['audio_file_path']
+        output_file = inputs.get('output_file', 'transcriptions/audio_wav_transcript_translate.txt')
+        
+        # Étape 1 : Transcription
+        transcription = self.stt_model.speak_to_microphone(stop_phrase="stop session", output_file=output_file)
+        # Étape 2 : Analyse de Sentiment
+        sentiment = self.sentiment_model.analyze_sentiment([transcription])
+        # Étape 3 : Génération de Réponse
+        response = self.generate_response_model.generate_response(sentiment, transcription)
+        # Étape 4 : Génération de date
+        timestamp = datetime.now()
+        
+        # Sauvegarder la sortie de chaque étape dans MongoDB
+        self.collection.insert_one({
+            "transcription": transcription,
+            "timestamp": timestamp,
+            "sentiment": sentiment,
+            "response": response
+        })
+        
+        return {
+            'transcription': transcription,
+            'timestamp': timestamp,
+            'sentiment': sentiment,
+            'response': response
+        }
+
+    def process(self, inputs):
+        return self._call(inputs)
+
+    @property
+    def input_keys(self):
+        return ['audio_file_path']
+    
+    @property
+    def output_keys(self):
+        return ['transcription', 'timestamp', 'sentiment', 'response']
+
+# Génération des modèles
+stt_model = Speech2TextModel()
 sentiment_model = SentimentAnalysisModel()
-sentiment_analysis_chain = SentimentAnalysisChain(sentiment_model=sentiment_model, collection=collection)
+generate_response_model = GenerateResponseModel()
 
-# Première chaîne pour la transcription et la traduction
-audio_file_path = 'audio_files/converted_2.wav'
-result_translation = transcription_translation_chain({'audio_file_path': audio_file_path})
+# Création de la chaîne
+chain = DeepChain(
+    stt_model=stt_model,
+    sentiment_model=sentiment_model,
+    generate_response_model=generate_response_model,
+    collection=collection,
+    speech_key=speech_key,
+    region = region,
+    mongodb_uri = mongodb_uri
+)
 
-print("Transcription:", result_translation['transcription'])
-print("Translation:", result_translation['translation'])
+# Fichier audio en entrée
+input_audio = "audio_files/converted_2.wav"
 
-# Deuxième chaîne pour l'analyse de sentiment
-result_sentiment = sentiment_analysis_chain({
-    'transcription': result_translation['transcription'],
-    'translation': result_translation['translation']
-})
-
-print("Sentiment:", result_sentiment['sentiment'])
-
-# Pour run ce fichier, il faut effectuer ces commandes:
-# cd /home/utilisateur/Documents/DEV-IA/projets/DEEP_LEARNING/NLP_banking
-# python -m models.langchain
+# Exécution de la chaîne
+results = chain.process({'audio_file_path': input_audio})
+print(results)
--- models/models.py
+++ models/models.py
@@ -1,88 +1,184 @@
-from transformers import pipeline
 import os
 from dotenv import load_dotenv
-import threading
-from pymongo import MongoClient
 import azure.cognitiveservices.speech as speechsdk
+from pymongo import MongoClient
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from transformers import pipeline
+import openai
+import pyaudio
+import wave
+import threading
+import uuid
+import subprocess
 
-# First model: Speech2Text (Azure)
-class SpeechToTextModel:
+# From Mic
+class Speech2TextModel:
     def __init__(self):
-
-        # Load environment variables
         load_dotenv()
-        self.speech_key = os.getenv("api_key")
-        self.service_region = os.getenv("region")
-        self.mongodb_uri = os.getenv("mongodb_uri")
+        self.speech_key = os.getenv("speech_key")
+        self.region = os.getenv("region")
+        self.recording = False
 
-        if not all([self.speech_key, self.service_region, self.mongodb_uri]):
-            raise ValueError("One or more environment variables are missing")
+    @staticmethod
+    def generate_unique_filename():
+        unique_id = uuid.uuid4()
+        return str(unique_id)
+    
+    def _check_mic_available(self):
+        p = pyaudio.PyAudio()
+        num_devices = p.get_host_api_info_by_index(0).get('deviceCount')
+        mic_available = False
+        for i in range(num_devices):
+            device_info = p.get_device_info_by_host_api_device_index(0, i)
+            if device_info.get('maxInputChannels') > 0:
+                print(f"Input Device id {i} - {device_info.get('name')}")
+                mic_available = True
+        p.terminate()
+        return mic_available
 
-        # MongoDB setup
-        self.cluster = MongoClient(self.mongodb_uri)
-        self.db = self.cluster["AzureSpeechToText"]
-        self.collection = self.db["transcriptions"]
+    def is_mic_available(self):
+        return self._check_mic_available()
 
-    # Transcribe Speech to Text (French)
-    def transcribe(self, audio_file_path, output_file):
-        if not os.path.isfile(audio_file_path):
-            raise FileNotFoundError(f"Audio file not found: {audio_file_path}")
+    def record_audio(self, output_file):
+        chunk = 1024
+        sample_format = pyaudio.paInt16
+        channels = 1
+        fs = 44100  # samples per second
+        p = pyaudio.PyAudio()  # creates an interface to PortAudio
+        
+        # Vérification des périphériques audio disponibles
+        if not self._check_mic_available():
+            print("Aucun microphone disponible. Tentative d'activation via PulseAudio...")
+            try:
+                subprocess.run(["pactl", "load-module", "module-loopback"], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Erreur lors de l'activation du micro: {e}")
+                raise Exception("Aucun microphone disponible après tentative d'activation.")
+                return  # Arrêtez l'enregistrement si le micro ne peut pas être activé
+            
+        stream = p.open(format=sample_format,
+                        channels=channels,
+                        rate=fs,
+                        frames_per_buffer=chunk,
+                        input=True)
+        frames = []  # initialize array to store frames
+        print("Recording audio...")
+        while self.recording:
+            data = stream.read(chunk)
+            frames.append(data)
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        print("Saving audio...")
+        wf = wave.open(output_file, "wb")
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(fs)
+        wf.writeframes(b''.join(frames))
+        wf.close()
 
-        speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.service_region)
+    def speak_to_microphone(self, stop_phrase, output_file):
+        speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.region)
         speech_config.speech_recognition_language = "fr-FR"
-        
-        audio_config = speechsdk.audio.AudioConfig(filename=audio_file_path)
-        recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-        print("Reconnaissance vocale en cours à partir du fichier audio...")
+        print('Speak into your microphone. Say "stop session" to end the recording session.')
 
-        all_results = []
+        self.recording = True
 
-        done = threading.Event()
+        # Generate unique filenames and save them in the corresponding folders
+        base_filename = self.generate_unique_filename()
+        audios_folder = "audios/"
+        transcripts_folder = "transcripts/"
+        # Ensure the directories exist
+        os.makedirs(audios_folder, exist_ok=True)
+        os.makedirs(transcripts_folder, exist_ok=True)
+        audio_output_file = os.path.join(audios_folder, f"{base_filename}.wav")
+        text_output_file = os.path.join(transcripts_folder, f"{base_filename}.txt")
 
-        def recognized(evt):
-            all_results.append(evt.result.text)
-            print(f"Texte reconnu: {evt.result.text}")
+        # Start audio recording in a separate thread
+        audio_thread = threading.Thread(target=self.record_audio, args=(audio_output_file,))
+        audio_thread.start()
 
-        def stop_cb(evt):
-            print('CLOSING on {}'.format(evt))
-            done.set()
+        '''Speech to Text: Transcript'''
+        with open(text_output_file, "w", encoding="utf-8") as file:
+            while True:
+                # Wait for any audio signal
+                speech = speech_recognizer.recognize_once_async().get()
 
-        recognizer.recognized.connect(recognized)
-        recognizer.session_stopped.connect(stop_cb)
-        recognizer.canceled.connect(stop_cb)
+                if speech.reason == speechsdk.ResultReason.RecognizedSpeech:
+                    # Generate unique filename for text_transcription
+                    print('Recognized: {}'.format(speech.text))
+                    file.write(speech.text + "\n")
+                    if stop_phrase.lower() in speech.text.lower():
+                        print("Session ended by user.")
+                        self.recording = False  # stop recording
+                        audio_thread.join()  # wait for the audio thread to finish
+                        break
 
-        recognizer.start_continuous_recognition()
-        done.wait()
+                # Check if the mic-speech is recognized, if not, print an error message (including logs/details).
+                elif speech.reason == speechsdk.ResultReason.NoMatch:
+                    print("No speech could be recognized: {}".format(speech.no_match_details))
 
-        recognizer.stop_continuous_recognition()
-        
-        # Writing & Saving of the transcription
-        with open(output_file, "w", encoding='utf-8') as file:
-            file.write(" ".join(all_results))
-        return " ".join(all_results)
+                # Print an error message with details if transcript gets canceled for any reason
+                elif speech.reason == speechsdk.ResultReason.Canceled:
+                    cancellation_details = speech.cancellation_details
+                    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+                    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                        print('Error details: {}'.format(cancellation_details.error_details))
+                        print('Did you set the speech resource key and region values?')
 
-class TranslationModel:
-    def __init__(self):
-        self.translation_pipeline = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-en")
+        # Return the transcription content
+        with open(text_output_file, "r", encoding="utf-8") as file:
+            transcription = file.read()
+        return transcription
 
-    def translate(self, text):
-        result = self.translation_pipeline(text)
-        return result[0]['translation_text']
 
 class SentimentAnalysisModel:
     def __init__(self):
-        self.sentiment_pipeline = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-
+        load_dotenv()        
+        self.azure_text_analytics_key = os.getenv("AZURE_TEXT_ANALYTICS_KEY")
+        self.azure_text_analytics_endpoint = os.getenv("AZURE_TEXT_ANALYTICS_ENDPOINT")
+        # Configuration du client Azure Text Analytics
+        self.text_analytics_client = TextAnalyticsClient(
+            endpoint=self.azure_text_analytics_endpoint,
+            credential=AzureKeyCredential(self.azure_text_analytics_key)
+)
     def analyze_sentiment(self, text):
-        result = self.sentiment_pipeline(text)
-        return result[0]
+        # a méthode analyze_sentiment du client Azure Text Analytics attend une liste de documents comme entrée
+        result = self.text_analytics_client.analyze_sentiment(text)[0]
+        sentiment = result.sentiment
+        confidence_scores = {
+            "positive": result.confidence_scores.positive,
+            "neutral": result.confidence_scores.neutral,
+            "negative": result.confidence_scores.negative
+        }
+        print(f"Sentiment Analysis: {result}")
+        return {"sentiment": sentiment, "scores": confidence_scores}
 
 class GenerateResponseModel:
-    def __init__(self):
-        self.generate_response_pipeline = pipeline('text-generation', model='gpt2')
+    def __init__(self) -> None:
+        load_dotenv()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.api_base = os.getenv("OPENAI_API_BASE")
+        self.api_deployment = os.getenv('OPENAI_DEPLOYMENT')
+        self.api_version = os.getenv('OPENAI_VERSION')
 
-    def generate_response(self, sentiment_result, text):
-        prompt = f"You work as an bank employee, your role is to take care of custommers. Based on the sentiment analysis result: {sentiment_result} of this review posted by a client: {text}, generate a short helpful response for the client:"
-        response = self.generate_response_pipeline(prompt, max_length=200, truncation=True, num_return_sequences=1)
-        return response[0]['generated_text']
+    def generate_response(self, sentiment, text):
+        prompt = f"Voici la transcription d'un message vocal laissé par un client auprès de sa banque. Analyse la transcription suivante et résume en quelques mots pourquoi le client est-il insatisfait. Transcription: {text}"
+        # Appel à l'API OpenAI pour générer une réponse basée sur le modèle GPT-3.5-turbo.
+        # Le modèle est configuré avec un ensemble de messages, dont un message système pour configurer le contexte et un message utilisateur contenant l'invite créée ci-dessus.
+        response = openai.ChatCompletion.create(
+            engine=self.api_deployment,  # Spécifie l'instance du modèle déployée à utiliser
+            model="gpt-3.5-turbo",  
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},  # Message système pour définir le rôle de l'assistant
+                {"role": "user", "content": prompt}  # Message utilisateur contenant l'invite
+            ],
+            max_tokens=100  # Limite le nombre de tokens dans la réponse générée à 100
+        )
+        # Extraction de la réponse générée via l'appel à l'API.
+        llm_response = response['choices'][0]['message']['content'].strip()
+        print("Réponse du modèle OpenAI : {}".format(llm_response))
+        return llm_response
